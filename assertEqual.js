df4ab3dc4ac916f7188af7acb687a2bf915be7d8
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-02 16:48:42 -0700

    assert Equal

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+if (actual === expected) {
+  console.log("Assertion Passed 🙂🙂: " + actual + " === " + expected);
+} else {console.log("Assertion Failed 👎👎: " + actual + " !== " + expected);
+}
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1,-1)
