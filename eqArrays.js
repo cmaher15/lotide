63795600212c9f8d3bfeadd24140fff78106e617
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-03 21:40:13 -0700

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed 🙂🙂: ${actual} === ${expected}`);
+  } else {console.log(`Assertion Failed 👎👎: ${actual} !== ${expected}`);
+  }
+  };
+  
+  assertEqual("Lighthouse Labs", "Lighthouse Labs");
+  assertEqual(1,-1)
+
+  const eqArrays = function (array1, array2) {
+    if (array1.length !== array2.length){
+      return false
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false )
