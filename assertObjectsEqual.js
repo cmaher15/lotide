6a0ac0e405006ae3c7d0640eb5866e78305c4e72
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-05 12:58:13 -0700

    final changes

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -20,19 +20,22 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const assertObjectsEqual = function(actual, expected) {
+const assertObjectsEqual = function(object1, object2) {
   const inspect = require('util').inspect;
-  if (eqObjects(actual, expected)) {
-    console.log(`Assertion passed 🙂🙂: ${actual} === ${expected}`);
+  if (eqObjects(object1, object2)) {
+    console.log(`Assertion passed 🙂🙂: ${inspect(object1)} === ${inspect(object2)}`);
   } else {
-    console.log(`Assertion Failed 👎👎: ${actual} !== ${expected}`);
+    console.log(`Assertion Failed 👎👎: ${inspect(object1)} !== ${inspect(object2)}`);
   }
-  console.log(`Example label : ${inspect(actual)}`);
 };
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const bc = { b: "4", c: "5", d: "6"};
 const cd = { b: "4", c: "6", d: "5", e: "8"};
-assertObjectsEqual(eqObjects(ab, ba), true);
-assertObjectsEqual(eqObjects(bc, cd), false);
+const xy = { x: "8", y: "y"};
+const yz = { y: "9", z: "10"};
+assertObjectsEqual(ab, ba);
+assertObjectsEqual(bc, cd);
+assertObjectsEqual(xy, yz);
+
