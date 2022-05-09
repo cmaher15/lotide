c8d4e38f5db4f507c416ea69fe14be911e67c0ae
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-09 16:27:07 -0700

    changed format to mocha/chai testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,17 @@
-
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 6 for [6 , 7, 8. 9]", () => {
+    assert.strictEqual(head([6, 7, 8, 9]), 6);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
