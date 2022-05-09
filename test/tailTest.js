edced3f91a953b8ee8be0f7f175571e0e526ee31
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-09 16:07:34 -0700

    fixed refactoring test for tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,6 @@
 const assertEqual = require('../assertEqual');
-const tail  = require('../tail');
+const tail = require('../tail');
 
-console.log(tail([2, 4, 6, 8, 9, 10]));
 const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
 assertEqual(words.length, 3);
