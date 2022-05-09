3233c990318ac528fa81547749b5b8547bf3c3d4
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-09 16:06:47 -0700

    refactoring test for assertArraysEqual.js

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require("../assertArraysEqual")
+
+
+assertArraysEqual([1,2,3,4], [1,2,3,4]);
+assertArraysEqual([1,2,3,4], [1,2,3,4]);
+assertArraysEqual([1,2,3,4], [1,2,3,4]);
+assertArraysEqual(['Hello', 'how', 'are', 'you'], ['Hello', 'how', 'are', 'you']);
