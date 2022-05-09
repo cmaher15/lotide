b2e0e99bd4d4e28b829163d65690093a52f70259
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-09 13:09:34 -0700

    creating test file for head.js code

--- head.js
+++ head.js
@@ -1,21 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed 🙂🙂: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed 👎👎: ${actual} !== ${expected}`);
-  }
-};
-  
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1,-1);
+const assertEqual = require('./assertEqual');
 
 const head = (array) => {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
 module.exports = {
+  head,
   assertEqual
-  }
+};
