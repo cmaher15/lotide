bf5aea873a4196359c8f094b01e83b0b5ea7ca1a
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-09 15:36:52 -0700

    fixed and tidied up refactoring on eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed 🙂🙂: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed 👎👎: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1,-1);
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -22,13 +11,4 @@ const eqArrays = function(array1, array2) {
 };
 
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-
-module.exports = {
-  assertEqual,
-  eqArrays
-  }
+module.exports = eqArrays;
