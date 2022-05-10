30fafcb1415917353821d13dc4942a5fd5d71574
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-09 21:29:36 -0700

    refactored and refined for mocha testing

--- countLetters.js
+++ countLetters.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed 🙂🙂: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed 👎👎: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(allLetters) {
   const output = {};
   const newLetters = allLetters.split(" ").join("");
@@ -19,17 +11,4 @@ const countLetters = function(allLetters) {
   return output;
 };
 
-console.log(countLetters("ceilidhe"));
-console.log(countLetters("LHL"));
-console.log(countLetters("lighthouse in the house"));
-
-//ASSERT ARRAY TEST NEEDED
-const result1 = countLetters("ceilidhe");
-const result2 = countLetters("Lighthouse in the house");
-assertEqual(result1.e, 2);
-assertEqual(result2.h, 4);
-
-module.exports = {
-  assertEqual,
-  countLetters
-  }
+module.exports = countLetters
