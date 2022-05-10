3ca289ab4aa407afda466e2525fa8626bf98efda
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-09 20:21:47 -0700

    refactoring and editing for testing, findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed 🙂🙂: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed 👎👎: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(objects, movie) {
   let object = Object.entries(objects);
   for (const [key, value] of object) {
@@ -15,17 +7,7 @@ const findKeyByValue = function(objects, movie) {
   }
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
-module.exports = {
-  assertEqual,
-  findKeyByValue
-  }
+module.exports = findKeyByValue
+  
