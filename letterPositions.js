d2aa5034574952384e1eba3a0a6d0905aedfc0ae
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-09 21:21:33 -0700

    refactor and refined for mocha testing

--- letterPositions.js
+++ letterPositions.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log((`Assertion Passed 🙂🙂: ${array1} === ${array2}`));
-  } else {
-    console.log(`Assertion Failed 👎👎: ${array1} !== ${array2}`);
-  }
-};
-
 const letterPositions = function(allLetters) {
   const output = {};
   const string = allLetters.split(" ").join("");
@@ -35,16 +15,6 @@ const letterPositions = function(allLetters) {
   return output;
 };
 
-console.log(letterPositions("ceilidhe"));
-console.log(letterPositions("hello how are"));
 
 
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("ceilidhe").e, [1,7]);
-assertArraysEqual(letterPositions("ceilidhe").l, [3]);
-
-module.exports = {
-  eqArrays,
-  assertArraysEqual,
-  letterPositions
-  }
+module.exports = letterPositions;
