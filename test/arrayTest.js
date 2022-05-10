5ba551af18243dc32b13962c683d59197e24e2c6
cmaher15 <ceilidhemaher@hotmail.com>
2022-05-09 20:25:53 -0700

    adding supporting documentation for lotide

--- test/arrayTest.js
+++
@@ -1,7 +0,0 @@
-const {assertArraysEqual} = require('../assertArraysEqual')
-describe('ArraysEqual', function() {
-  it('should return the appropriate assertion based on the input', function() {
-    assertArraysEqual([1,2,3], [1,2,3])
-  });
-});
-
